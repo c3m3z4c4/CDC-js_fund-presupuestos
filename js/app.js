f10e76a64d1092b43568e3bf22650e1a588ab9ad
C3M3Z4C4 <cmeza@mezadigital.com>
2023-02-26 14:12:02 -0600

    [AVANCE 4] Ultimos toques previa entrega

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
-let presupuesto = 20000;
-let porcentajeEgreso = 0.25
 
-// let egresos = [
-//   {desc:'Renta', ammount:900},
-//   {desc:'Ropa', ammount:400}
-// ];
 
-//   let ingresos = [
-//     { desc:'Quincena', ammount:9000},
-//     {desc:'Venta', ammount:400}
-//   ];
+let ingresos =[
+  // {
+  //   id:1,
+  //   descripcion: "Salario",
+  //   valor: 40000
+  // },
+  // {
+  //   id:2,
+  //   descripcion: "Venta Auto",
+  //   valor: 60000
+  // },
+  // {
+  //   id:3,
+  //   descripcion: "Regalias",
+  //   valor: 15000
+  // },
+];
 
-let ingresos =  new Array (new Ingreso("Salario", 20000),new Ingreso("Venta auto",
-  50000))
-let egresos =  new Array(new Egreso("Renta", 4000),new Egreso("Ropa",
-  600))
 
+let egresos = [
+  // {
+  //   id:1,
+  //   descripcion: "Renta",
+  //   valor: 8000
+  // },
+  // {
+  //   id:2,
+  //   descripcion: "Ropa",
+  //   valor: 600
+  // },
+  // {
+  //   id:3,
+  //   descripcion: "Luz",
+  //   valor: 1230
+  // },
+];
 const totalIngresos = () => {
 
   let totalIngresos = 0;
@@ -59,12 +79,117 @@ const cargarCabecero = () => {
   let presupuesto = totalIngresos() - totalEgresos();
   let porcentajeEgreso = totalEgresos()/totalIngresos();
 
-  console.log(formatoMoneda(presupuesto));
-  console.log(formatoPorcentaje(porcentajeEgreso));
-  console.log(formatoMoneda(totalIngresos()));
-  console.log(formatoMoneda(totalEgresos()));
+  // Cargando Presupuesto cabecera
+  let presup=document.querySelector("#presupuesto");
+  presup.innerHTML=formatoMoneda(presupuesto);
 
+//cargando porcentajes de egresos
+  let percent=document.querySelector("#porcentaje");
+  percent.innerHTML=formatoPorcentaje(porcentajeEgreso);
+
+// Cargando ingresos a la cabecera
+
+  let ingress=document.querySelector("#ingresos");
+  ingress.innerHTML=formatoMoneda(totalIngresos());
+  
+  //Cargando egresos a la cabecera
+
+  let egress=document.querySelector("#egresos");
+  egress.innerHTML=formatoMoneda(totalEgresos());
 }
 
 
-cargarCabecero()
+// Funcion creadora del ingreso HTML
+
+const crearIngresoHTML = (ingreso) => {
+  let ingresoHTML = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+      <div class="elemento_valor">+${ingreso.valor}</div> 
+      <div class="elemento_eliminar">
+        <button class="elemento_eliminar--btn" ><ion-icon name="close-circle-outline"></ion-icon></button>
+      </div> 
+    </div>
+  </div>`;
+  //  onclick="${eliminarIngreso(ingreso.id)}"
+  return ingresoHTML
+}
+const crearEgresoHTML = (egreso) => {
+  let egresoHTML = `<div class="elemento limpiarEstilos">
+    <div class="elemento_descripcion">${egreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+      <div class="elemento_valor">-${egreso.valor}</div> 
+      <div class="elemento_eliminar">
+        <button class="elemento_eliminar--btn"><ion-icon name="close-circle-outline" ></ion-icon></button>
+      </div> 
+    </div>
+  </div>`;
+  // onclick="${eliminarEgreso(egreso.id)}
+  return egresoHTML
+}
+
+
+
+// Funcion para cargar ingresos
+const cargarIngresos =()=>{
+  let ingresosHTML =new Array()
+  console.log(ingresos)
+  for(let i=0; i<ingresos.length; i++){
+
+    ingresosHTML.push(crearIngresoHTML(ingresos[i]));
+  }
+  document.querySelector("#lista-ingresos").innerHTML=ingresosHTML.join('');
+
+}
+
+// Funcion para cargar egresos
+const cargarEgresos =()=>{
+    let egresosHTML = [];
+    for(let i=0; i<egresos.length; i++){
+      egresosHTML.push(crearEgresoHTML(egresos[i]));
+    };
+    document.querySelector("#lista-egresos").innerHTML=egresosHTML.join('');
+}
+
+const eliminarIngreso = (id) => {
+  let indiceEliminar = ingresos.findIndex((el)=>{id===el.id})
+  ingresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarIngresos();
+}
+
+const eliminarEgreso = (id) => {
+  let indiceEliminar = egresos.findIndex((el)=>{id===el.id})
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarEgresos();
+}       
+
+// Funcion para agregar dato
+
+const agregarDato = () => {
+  let forma = document.getElementById('forma');
+  let tipo = document.getElementById('tipo').value;
+  let descripcion= document.getElementById('descripcion').value;
+  let valor = document.getElementById('valor').value;
+  console.log(tipo, descripcion, valor)
+  if(descripcion!="" && valor!=""){
+      if(tipo='ingreso'){
+        console.log(tipo, descripcion, parseInt(valor, 10))
+        ingresos.push(new Ingreso(descripcion, parseInt(valor, 10)));
+      }else if(tipo=='egreso'){
+        console.log(tipo, descripcion, parseInt(valor, 10))
+        egresos.push(new Egreso(descripcion, parseInt(valor, 10)));
+      }
+  }
+  cargarCabecero()
+  cargarIngresos()
+
+}
+
+// Cargando la aplicacion
+function cargarApp(){
+  cargarCabecero()
+  cargarIngresos()
+  cargarEgresos()
+}
